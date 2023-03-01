12f0b0c960eaa85b11cf4695825e0a90be2c38c1
BinbinvoGaming <123051097+DeletedUserCodeblockfuits@users.noreply.github.com>
2023-03-01 18:49:44 +0700

    Update and rename README.md to Kaizenofficiall/Protected.lua

---
+++ Kaizenofficiall/Protected.lua
@@ -0,0 +1,2 @@
+getgenv().key = "KEY_BinbinvoGamingHubNew";
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/DeletedUserCodeblockfuits/DeletedUserCode/main/Kaizenofficiall/BinbinvoGaming/Premium/BinbinvoGamingHubPremium%20Script%20Hub.lua'),true))()
