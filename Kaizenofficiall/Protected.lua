398bba2e939bacf6c0660f2d294799b9f2f8742d
BinbinvoGaming <123051097+DeletedUserCodeblockfuits@users.noreply.github.com>
2023-03-02 06:18:17 +0700

    Update Protected.lua

--- Kaizenofficiall/Protected.lua
+++ Kaizenofficiall/Protected.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/DeletedUserCodeblockfuits/BinbinvoGamingHub/main/Kaizenofficiall/Loader.lua'))()
+getgenv().key = "KEY_BinbinvoGamingHubNew";
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/DeletedUserCodeblockfuits/DeletedUserCode/main/Kaizenofficiall/BinbinvoGaming/Premium/BinbinvoGamingHubPremium%20Script%20Hub.lua'),true))()
